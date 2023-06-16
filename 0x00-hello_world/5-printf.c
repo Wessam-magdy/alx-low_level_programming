0fdd5f5691fa84fa1d90a55931f6d776bce127d6
Wesam-magdy <wesa.mgdy@gmail.com>
2023-06-15 17:09:25 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: A C Pregram that print using printf function
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
