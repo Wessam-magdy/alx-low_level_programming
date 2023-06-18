d8e2e735b46b36633da476a00a1c81afc56b5afe
Wesam-magdy <wesa.mgdy@gmail.com>
2023-06-18 02:47:53 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description:  A C Program
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch <= 'a')
+	{
+		putchar(ch);
+		x--;
+	}
+	putchar('\n');
+	return (0);
+}
