fa34525427569cf42e67f8129727b5bbfd4940e8
Wesam-magdy <wesa.mgdy@gmail.com>
2023-06-18 02:37:20 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description:  A C Program
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x = 0;
+
+	while (x <= 9)
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
