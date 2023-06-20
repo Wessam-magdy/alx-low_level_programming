bd20433b1e343820404fc960eb9cd5bcfa546da4
Wesam-magdy <wesa.mgdy@gmail.com>
2023-06-20 04:48:03 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry Point
+ *
+ * Description: A C  program that print string
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char str = "_putchar"
+		for (int i = 0; i < 8 ; i++)
+		{
+			_putchar(str[i]);
+			i++;
+		}
+	_putchar("\n");
+	return (0);
+}
