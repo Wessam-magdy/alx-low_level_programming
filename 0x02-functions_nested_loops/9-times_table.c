ff53f47613eec99e55a6afa86b41d81b93833b3b
Wesam-magdy <wesa.mgdy@gmail.com>
2023-06-21 06:40:57 -0700

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * times_table- check if a charachter is lower case
+ *
+ * Description: A C  program that check charachters
+ * Return: 1 for success and . for fail
+ */
+
+void times_table(void)
+{
+	int num, mult, prod;
+
+	for (num = 0 ; num <= 9 ; num++)
+	{
+		_putchar(48);
+		for (mult = 1 ; mult <= 9 ; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			prod = num * mult;
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + 48);
+			_putchar((prod % 10) + 48;
+		}
+		_putchar('\n');
+	}
+}
