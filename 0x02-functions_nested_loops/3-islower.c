712f22dbbb4da2e40cc2f91393dc1fcded17618d
Wesam-magdy <wesa.mgdy@gmail.com>
2023-06-21 05:29:54 -0700

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - check if a charachter is lower case
+ *
+ * @c: the character to check
+ * Description: A C  program that check charachters
+ * Return: 1 for success and . for fail
+ */
+
+int _islower(int c)
+{
+	if (islower(c))
+		return (1);
+	else
+		return (0);
+}
