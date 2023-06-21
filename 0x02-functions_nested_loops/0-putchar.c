591bee910f9c23bb72cc4ad12b626d26e624bb89
Wesam-magdy <wesa.mgdy@gmail.com>
2023-06-21 04:35:31 -0700

    task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -12,11 +12,12 @@
 int main(void)
 {
 	char str[] = "_putchar";
-		for (int i = 0; i < 8 ; i++)
+	int i;
+		for (i = 0; i < 8 ; i++)
 		{
 			_putchar(str[i]);
 			i++;
 		}
-	_putchar("\n");
+	_putchar('\n');
 	return (0);
 }
