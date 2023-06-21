66571c183138e337b3a02a9f2520acb97716c15c
Wesam-magdy <wesa.mgdy@gmail.com>
2023-06-21 05:20:07 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - writes the character C to stdout
+ * @c the character to print
+ * @i the iteration
+ * Description: A C  program that  generate random number
+ *
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (c = 'a' ; c <= 'z' ; c++)
+		{
+			_putchar(c);
+		}
+		putchar('\n');
+	}
+}
