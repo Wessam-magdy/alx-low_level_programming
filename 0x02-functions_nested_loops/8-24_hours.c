c4e287905485610128e6c09c973eb569cc53376c
Wesam-magdy <wesa.mgdy@gmail.com>
2023-06-21 06:22:10 -0700

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * jack_bauer - writes the character C to stdout
+ * @c the character to print
+ * Description: A C  program that  generate random number
+ *
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+	int i = 00;
+	int j = 00;
+
+	for (i = 0 ; i < 24 ; i++)
+	{
+		for (j = 0 ; j <= 59 ; j++)
+			_putchar(i + ':' + j);
+	}
+
+}
