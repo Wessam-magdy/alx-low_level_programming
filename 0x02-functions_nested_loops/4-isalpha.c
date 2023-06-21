2e16d8d4433bb4a488bd4a1844a0fed1f10a9e58
Wesam-magdy <wesa.mgdy@gmail.com>
2023-06-21 05:50:29 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * __isalpha - check if a charachter is lower case
+ *
+ * @c: the character to check
+ * Description: A C  program that check charachters
+ * Return: 1 for success and . for fail
+ */
+
+int __isalpha(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
