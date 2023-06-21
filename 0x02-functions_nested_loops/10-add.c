4e0f434d9190170d41601c47c828715b37852aa2
Wesam-magdy <wesa.mgdy@gmail.com>
2023-06-21 06:45:30 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * add - to add 2 values
+ *
+ * @a: the character to sum
+ * @b: the second value to sum
+ * Description: A C  program that check charachters
+ * Return: 1 for success and . for fail
+ */
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
