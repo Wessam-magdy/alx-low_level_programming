60f407126f64e9c5272700efdf95e29eabcc5644
Wesam-magdy <wesa.mgdy@gmail.com>
2023-06-21 06:09:43 -0700

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit
+ *
+ * @c: the character to check
+ * Description: A C  program that check charachters
+ * Return: 1 for success and . for fail
+ */
+
+int print_last_digit(int c)
+{
+	c = c % 10;
+	return (c);
+}
