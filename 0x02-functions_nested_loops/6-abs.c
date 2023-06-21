8f6950d50c194079af2b4ecca5f7c64bc9f0c20d
Wesam-magdy <wesa.mgdy@gmail.com>
2023-06-21 06:03:53 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - get absolute value
+ *
+ * @c: the character to check
+ * Description: A C  program that check charachters
+ * Return: 1 for success and . for fail
+ */
+
+int _abs(int c)
+{
+	if (c < 0)
+		c = c * (-1);
+		return (c);
+}
