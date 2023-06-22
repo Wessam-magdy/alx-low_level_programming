db2dad2f92624177893a4d28dfd17a36e8a7be90
Wesam-magdy <wesa.mgdy@gmail.com>
2023-06-22 15:32:13 -0700

    task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+	 * _isdigit -  This is the preferred style for multi-line
+	 * comments in C source code.
+	 * Please use it consistently.
+	 *
+	 * Description:  A column of asterisks on the left side,
+	 * with beginning and ending almost-blank lines.
+	 * @c: inital par
+	 * Return: 1 if success and 0 if fail
+	 */
+
+int _isdigit(int c)
+{
+	if (c <= 48 && c >= 57)
+		return (1);
+	return (0);
+}
