c814dbac80505d20d2cbf9eda464ac3ff0edc99d
Wesam-magdy <wesa.mgdy@gmail.com>
2023-06-22 15:44:37 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *  print_numbers -  This is the preferred style for multi-line
+ * comments in C source code.
+ * Please use it consistently.
+ *
+ * Description:  A column of asterisks on the left side,
+ * with beginning and ending almost-blank lines.
+ *
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		_putchar(i + 48);
+	}
+}
