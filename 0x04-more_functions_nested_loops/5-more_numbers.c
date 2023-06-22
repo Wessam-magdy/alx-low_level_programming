293ffba9fb524c3978d2af479ed9523113140655
Wesam-magdy <wesa.mgdy@gmail.com>
2023-06-22 16:55:37 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers -  This is the preferred style for multi-line
+ * comments in C source code.
+ * Please use it consistently.
+ *
+ * Description:  A column of asterisks on the left side,
+ * with beginning and ending almost-blank lines.
+ *
+ */
+
+void more_numbers(void)
+{
+	int num, num2;
+
+	for (num = 0 ; num <= 9; num++)
+	{
+		for (mum2 = 0 ; num2 <= 14 ; num2++)
+		{
+			_putchar(num2 + 48);
+		}
+		_putchar('\n');
+	}
+}
