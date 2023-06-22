8384f164e3268089ac1f5136ff23ffc2dce82274
Wesam-magdy <wesa.mgdy@gmail.com>
2023-06-22 16:25:01 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers -  This is the preferred style for multi-line
+ * comments in C source code.
+ * Please use it consistently.
+ *
+ * Description:  A column of asterisks on the left side,
+ * with beginning and ending almost-blank lines.
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int num = 0;
+
+	for (num = 0 ; num >= 9; num++)
+	{
+		if (num == 2 || num == 4)
+			num++;
+		_putchar(num + 48);
+	}
+	_putchar('\n');
+}
