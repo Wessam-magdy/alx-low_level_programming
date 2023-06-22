9b4711ac04897df63f374903eac484a73a51d660
Wesam-magdy <wesa.mgdy@gmail.com>
2023-06-22 15:38:05 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *  mul -  This is the preferred style for multi-line
+ *  comments in C source code.
+ * Please use it consistently.
+ * Description:  A column of asterisks on the left side,
+ * with beginning and ending almost-blank lines.
+ * @a: inital par
+ * @b: second param
+ * Return: 1 if success and 0 if fail
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
