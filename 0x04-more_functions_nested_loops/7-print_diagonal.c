02a106aace77b74341d7c2ee8b312a83cdcc93db
Wesam-magdy <wesa.mgdy@gmail.com>
2023-06-22 17:31:12 -0700

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal -  This is the preferred style for multi-line
+ * comments in C source code.
+ * Please use it consistently.
+ *@n: param
+ * Description:  A column of asterisks on the left side,
+ * with beginning and ending almost-blank lines.
+ *
+ */
+
+void print_diagonal(int n)
+{
+	int postn, space;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (postn = 1 ; postn <= n ; postn++)
+		{
+			for (space = 1 ; space <= postn ; space++)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
