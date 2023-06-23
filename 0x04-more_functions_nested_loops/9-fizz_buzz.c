ac6cbbc1a20582ba4727c7dffc679202f510bec8
Wesam-magdy <wesa.mgdy@gmail.com>
2023-06-22 17:56:40 -0700

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * main -  This is the preferred style for multi-line
+ * comments in C source code.
+ * Please use it consistently.
+ *
+ * Description:  A column of asterisks on the left side,
+ * with beginning and ending almost-blank lines.
+ *
+ * Return: (0)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 1 ; num <= 100 ; num++)
+	{
+		if (num % 3 == 0 && !(num % 5 == 0))
+			printf("Fizz");
+		else if (num % 5 == 0 && !(num % 3 == 0))
+			printf("Buzz");
+		else if (num % 5 == 0 && num % 3 == 0)
+			printf("FizzBuss");
+		else
+			printf("%d", num);
+		if (num != 100)
+			printf(" ");
+		else
+			printf("\n");
+	}
+	return (0);
+}
