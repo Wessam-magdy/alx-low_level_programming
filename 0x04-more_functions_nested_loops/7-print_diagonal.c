22ee637f74ed743f5800d8ba474945ae7918ca75
Wesam-magdy <wesa.mgdy@gmail.com>
2023-06-22 17:37:07 -0700

    task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -18,9 +18,9 @@ void print_diagonal(int n)
 		_putchar('\n');
 	else
 	{
-		for (postn = 1 ; postn <= n ; postn++)
+		for (postn = 1 ; postn < n ; postn++)
 		{
-			for (space = 1 ; space <= postn ; space++)
+			for (space = 1 ; space < postn ; space++)
 			{
 				_putchar(' ');
 			}
