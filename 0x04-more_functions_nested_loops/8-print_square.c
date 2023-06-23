7a5fde9eacb6cfb818469e03cb41a9e13ddbe7a1
Wesam-magdy <wesa.mgdy@gmail.com>
2023-06-22 17:44:34 -0700

    task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_square -  This is the preferred style for multi-line
+ * comments in C source code.
+ * Please use it consistently.
+ * @size: param
+ * Description:  A column of asterisks on the left side,
+ * with beginning and ending almost-blank lines.
+ *
+ */
+
+void print_square(int size)
+{
+	int c, r;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (r = 0 ; r < size ; r++)
+		{
+			for (c = 0; c < size; c++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+
+}
