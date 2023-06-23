13a1189762260e46a39ba2da79c8da0b412947dd
Wesam-magdy <wesa.mgdy@gmail.com>
2023-06-22 17:21:01 -0700

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -14,13 +14,15 @@ void print_line(int n)
 {
 	int count = 0;
 
-	if (n > 0)
+	if (n <= 0)
+		_putchar('\n');
+	else
 	{
 		do {
 			_putchar('-');
 			count++;
 		} while (count < n)
-	}
 	_putchar('\n');
+	}
 
 }
