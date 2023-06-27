e6517b51825d339c0206de642944b043dada8b96
Wesam-magdy <wesa.mgdy@gmail.com>
2023-06-27 14:06:04 -0700

    task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcpy -  This is the preferred style for multi-line
+ * comments in C source code.
+ * Please use it consistently.
+ * Description:  A column of asterisks on the left side,
+ * with beginning and ending almost-blank lines.
+ * @dest:  int param
+ * @src: param
+ * Return: 1 if success and 0 if fail
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = -1;
+
+	do {
+		i++;
+		dest[i] = src[i];
+	} while (src[i] != "\0");
+	return (dest);
+}
