9e3164acad7cd74a960c08112fedc00d1f795470
Wesam-magdy <wesa.mgdy@gmail.com>
2023-06-27 13:16:57 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts -  This is the preferred style for multi-line
+ * comments in C source code.
+ * Please use it consistently.
+ * Description:  A column of asterisks on the left side,
+ * with beginning and ending almost-blank lines.
+ * @str:  int param
+ * Return: 1 if success and 0 if fail
+ */
+void _puts(char *str)
+{
+	while (*str != '/0')
+	{
+		_putchar(*str + 0);
+		str++;
+	}
+	_putchar('\n');
+}
