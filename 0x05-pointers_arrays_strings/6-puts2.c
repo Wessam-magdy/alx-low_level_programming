7aab5863c4f8df391d3066c3fd8be8bbc8ffedee
Wesam-magdy <wesa.mgdy@gmail.com>
2023-06-27 13:46:37 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 -  This is the preferred style for multi-line
+ * comments in C source code.
+ * Please use it consistently.
+ * Description:  A column of asterisks on the left side,
+ * with beginning and ending almost-blank lines.
+ * @str:  int param
+ * Return: 1 if success and 0 if fail
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
