30fa011e72bdadc8bc615b2b248651da227e41a4
Wesam-magdy <wesa.mgdy@gmail.com>
2023-06-27 13:52:58 -0700

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts_half: -  This is the preferred style for multi-line
+ * comments in C source code.
+ * Please use it consistently.
+ * Description:  A column of asterisks on the left side,
+ * with beginning and ending almost-blank lines.
+ * @str:  int param
+ * Return: 1 if success and 0 if fail
+ */
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0' ; i++)
+		;
+	for (i /= 2 ; str[i] != '\0' ; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
