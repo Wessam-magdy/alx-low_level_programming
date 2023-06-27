8ffc50ded889841ddfbeb645ba65f6575611f444
Wesam-magdy <wesa.mgdy@gmail.com>
2023-06-27 12:51:59 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,16 +1,16 @@
 #include "main.h"
+
 /**
-         * reset_to_98 -  This is the preferred style for multi-line
-         * comments in C source code.
-         * Please use it consistently.
-         *
-         * Description:  A column of asterisks on the left side,
-         * with beginning and ending almost-blank lines.
-         * @*n  int param
-         * Return: 1 if success and 0 if fail
-         */
+ * reset_to_98 -  This is the preferred style for multi-line
+ * comments in C source code.
+ * Please use it consistently.
+ * Description:  A column of asterisks on the left side,
+ * with beginning and ending almost-blank lines.
+ * @*n:  int param
+ * Return: 1 if success and 0 if fail
+ */
+
 void reset_to_98(int *n)
 {
 	*n = 98;
-	
 }
