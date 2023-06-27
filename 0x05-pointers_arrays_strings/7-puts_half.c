b23af410136f6694cbc518d68857f877f2b5a65e
Wesam-magdy <wesa.mgdy@gmail.com>
2023-06-27 13:56:59 -0700

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts_half: -  This is the preferred style for multi-line
+ * puts_half -  This is the preferred style for multi-line
  * comments in C source code.
  * Please use it consistently.
  * Description:  A column of asterisks on the left side,
