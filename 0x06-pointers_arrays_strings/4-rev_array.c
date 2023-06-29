2bb6352e814e9c53e90cdf786d3471c808087751
Wesam-magdy <wesa.mgdy@gmail.com>
2023-06-29 03:58:26 -0700

    task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * reverse_array -  This is the preferred style for multi-line
+ * comments in C source code.
+ * Please use it consistently.
+ * Description:  A column of asterisks on the left side,
+ * with beginning and ending almost-blank lines.
+ * @a:  int param
+ * @n: param
+ * Return: 1 if success and 0 if fail
+ */
+void reverse_array(int *a, int n)
+{
+	int i, j, t;
+
+	for (i = 0; j < (n - 1) ; i < j ; i++, j--)
+	{
+		t = a[i];
+		a[i] = a[j];
+		a[j] = t;
+	}
+
+}
