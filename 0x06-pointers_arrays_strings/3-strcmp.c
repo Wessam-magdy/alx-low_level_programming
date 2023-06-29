eb0deeb6c6e0856a47182b35960be1455b64643e
Wessam-magdy <133385797+Wessam-magdy@users.noreply.github.com>
2023-06-29 14:07:08 +0300

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,26 +1,17 @@
 #include "main.h"
 
 /**
- * _strcmp -  This is the preferred style for multi-line
- * @s1:  int param
- * @s2: param
- * Return: param
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: 0 if strings are equal, otherwise difference between first
+ * different characters
  */
-
 int _strcmp(char *s1, char *s2)
 {
-	int equal = 0;
+	int i;
 
-	while (*s1)
-	{
-		if (*s1 != *s2)
-		{
-			equal = ((int)*s1 - 48) - ((int)*s2 - 48);
-			break;
-		}
-		s1++;
-		s2++
-	}
-	return (equal);
-
+	for (i = 0; s1[i] && s2[i] && s1[i] == s2[i]; i++)
+		;
+	return (s1[i] - s2[i]);
 }
